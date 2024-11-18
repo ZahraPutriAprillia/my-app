9bead84f45a7c0dad7ceef34a3176fc67bfcbd14
ZahraPutriAprillia <zahr22178si@student.nurulfikri.ac.id>
2024-11-18 23:40:00 +0700

    first commit

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,86 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'SHRT-1-01-MEN-001',
+      slug: 'casual-oxford-shirt-men',
+      name: 'Casual Oxford Shirt',
+      category: 'Men\'s Wear',
+      price: 450_000,
+      stock: 200,
+      imageUrl: '/assets/images/CASUAL-OXFORD-SHIRT-MEN.jpeg',
+      description: `A timeless Casual Oxford Shirt with a slim fit design, perfect for both formal and casual occasions. Made from premium cotton for maximum comfort and durability.`
+    },
+    {
+      id: 'SHRT-1-02-MEN-002',
+      slug: 'formal-slim-fit-shirt-men',
+      name: 'Formal Slim Fit Shirt',
+      category: 'Men\'s Wear',
+      price: 199_000,
+      stock: 89,
+      imageUrl: '/assets/images/FORMAL-SLIM-FIT-SHIRT-MEN.jpeg',
+      description: `Elevate your wardrobe with this Formal Slim Fit Shirt. Featuring a crisp collar and modern fit, it’s perfect for business meetings or formal events.`
+    },
+    {
+      id: 'SHRT-1-03-WOMEN-001',
+      slug: 'casual-linen-shirt-women',
+      name: 'Casual Linen Shirt',
+      category: 'Women\'s Wear',
+      price: 799_000,
+      stock: 25,
+      imageUrl: '/assets/images/CASUAL-LINEN-SHIRT-WOMEN.jpeg',
+      description: `A breezy Casual Linen Shirt designed for ultimate comfort. The lightweight fabric and relaxed fit make it ideal for warm weather or a casual day out.`
+    },
+    {
+      id: 'SHRT-1-04-WOMEN-002',
+      slug: 'elegant-blouse-shirt-women',
+      name: 'Elegant Blouse Shirt',
+      category: 'Women\'s Wear',
+      price: 449_000,
+      stock: 201,
+      imageUrl: '/assets/images/ELEGANT-BLOUSE-SHIRT-WOMEN.jpeg',
+      description: `This Elegant Blouse Shirt combines a sophisticated design with soft, flowy fabric. Perfect for pairing with trousers or skirts for a chic look.`
+    },
+    {
+      id: 'SHRT-1-05-MEN-003',
+      slug: 'flannel-check-shirt-men',
+      name: 'Flannel Check Shirt',
+      category: 'Men\'s Wear',
+      price: 299_000,
+      stock: 30,
+      imageUrl: '/assets/images/FLANNEL-CHECK-SHIRT-MEN.jpeg',
+      description: `Add a rugged touch to your wardrobe with this Flannel Check Shirt. Its soft fabric and classic pattern make it a versatile piece for layering or standalone wear.`
+    },
+    {
+      id: 'SHRT-1-06-WOMEN-003',
+      slug: 'oversized-cotton-shirt-women',
+      name: 'Oversized Cotton Shirt',
+      category: 'Women\'s Wear',
+      price: 359_000,
+      stock: 20,
+      imageUrl: '/assets/images/OVERSIZED-COTTON-SHIRT-WOMEN.jpeg',
+      description: `An Oversized Cotton Shirt that combines comfort with modern style. Pair it with jeans or leggings for a relaxed yet fashionable look.`
+    },
+    {
+      id: 'SHRT-1-07-MEN-004',
+      slug: 'short-sleeve-linen-shirt-men',
+      name: 'Short Sleeve Linen Shirt',
+      category: 'Men\'s Wear',
+      price: 300_000,
+      stock: 22,
+      imageUrl: '/assets/images/SHORT-SLEEVE-LINEN-SHIRT-MEN.jpeg',
+      description: `Stay cool and stylish with this Short Sleeve Linen Shirt. Designed with breathable fabric and a relaxed fit, it's perfect for casual outings or summer days.`
+    },
+    {
+      id: 'SHRT-1-08-WOMEN-004',
+      slug: 'striped-button-up-shirt-women',
+      name: 'Striped Button-Up Shirt',
+      category: 'Women\'s Wear',
+      price: 289_000,
+      stock: 50,
+      imageUrl: '/assets/images/STRIPED-BUTTON-UP-SHIRT-WOMEN.jpeg',
+      description: `Add a touch of elegance to your wardrobe with this Striped Button-Up Shirt. Featuring a classic stripe pattern and lightweight material, it's great for both work and casual wear.`
+    }
+    
+    
+  ]
+}
